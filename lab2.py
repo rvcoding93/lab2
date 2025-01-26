e3e7d5d9d0688be8b89ed8d4aa27d35efebfa52f
Ronnie Vincenty <ronnievcoding@gmail.com>
2025-01-26 13:35:36 -0500

    update

--- lab2.py
+++ lab2.py
@@ -1,1 +1,10 @@
-#tst
+# Program Name: Lab2.py
+# Course: IT1114/Section  W03
+# Student Name: Ronnie Vincenty
+# Assignment Number: Lab 2
+# Due Date: 01/26/2025
+# Purpose: This program calculates the total food cost for the KSU CCSE hackathon.
+#          It computes the number of whole pizzas needed, their cost, the salad cost,
+#          applies applicable discounts, calculates the delivery fee, and displays
+#          the final amount due.
+# Resources Used: No external sources were used. Only personal knowledge and course materials.
